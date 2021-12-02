df6c01c2920c312e4cc301471d48c374eba6662a
shoaibpkkhan123 <89197091+shoaibpkkhan123@users.noreply.github.com>
2021-12-02 13:49:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload() {
+}
+
+function setup(){
+var canvas =  createCanvas(700,600);
+canvas.parent('canvas');
+
+video = createCapture(VIDEO);
+video.size(700, 600);
+video.hide();
+
+}
